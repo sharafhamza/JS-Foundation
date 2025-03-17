14c71c1b9eeb38a6ce71b9ba7da143f062704268
sharafhamza <kingsharaf14@gmail.com>
2025-03-17 17:16:31 +0600

    changes

---
+++ Primitive.js
@@ -0,0 +1,32 @@
+// Number
+let age = 25;
+let price = 99.99;
+
+// BigInt
+let bigNumber = 123456789012345678901234567890n;
+
+// String
+let name = "John";
+let greeting = `Hello, ${name}`;
+
+// Boolean
+let isOnline = true;
+let hasPermission = false;
+
+// Undefined
+let notDefined;
+
+// Null
+let emptyValue = null;
+
+// Symbol
+let uniqueKey = Symbol("id");
+
+// Output all values
+console.log("Number:", age, price);
+console.log("BigInt:", bigNumber);
+console.log("String:", greeting);
+console.log("Boolean:", isOnline, hasPermission);
+console.log("Undefined:", notDefined);
+console.log("Null:", emptyValue);
+console.log("Symbol:", uniqueKey);
