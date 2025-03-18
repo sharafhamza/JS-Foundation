4798007737b8b30e7716625d3ce6e957b0a2900d
sharafhamza <kingsharaf14@gmail.com>
2025-03-18 18:03:36 +0600

    changes

--- test.js
+++ test.js
@@ -20,9 +20,70 @@
 //   double.push(multiply);
 // }
 // console.log(double);
-function convertNumber(str) {
-  const num = Number(str);
-  return isNaN(num) ? "Not A Number" : num;
-}
 
-console.log(convertNumber("5"));
+// Test 1
+// function convertNumber(str) {
+//   const num = Number(str);
+//   return isNaN(num) ? "Not A Number" : num;
+// }
+
+// console.log(convertNumber("5"));
+
+// Test 2
+// function flipBoolean(input) {
+//   return !Boolean(input);
+// }
+
+// console.log(flipBoolean(false));
+
+// Test 3
+// function whatAmI(value) {
+//   if (typeof input === "number") {
+//     return "I'm a number!";
+//   } else if (typeof input === "string") {
+//     return "I'm a string!";
+//   }
+//   return "I am the type of " + typeof value;
+// }
+
+// console.log(whatAmI(true));
+
+// Test 4
+// function isItTruthy(input) {
+//     if (input) {
+//         return "It's truthy!";
+//     }
+
+//     return "It's falsey!";
+// }
+
+// Test 5
+// function arrayNumber(arr) {
+//   return arr.filter((item) => typeof item === "number");
+// }
+
+// const mixedArr = ["hello", 2, 4, "sharaf", "al", "hamza"];
+
+// console.log(arrayNumber(mixedArr));
+// console.log(mixedArr);
+
+// Test 6
+// function reverseArray(arr) {
+//   return arr.reverse();
+// }
+
+// const mixedArr = ["hello", 2, 4, "sharaf", "al", "hamza"];
+// console.log(reverseArray(mixedArr));
+
+const numbers = [2, 3, 4, 5, 6, 1, 44, 55];
+
+let largeNumber = 0;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > largeNumber) {
+    largeNumber = numbers[i];
+  }
+
+  return largeNumber;
+}
+console.log(largeNumber);
+console.log("hello");
