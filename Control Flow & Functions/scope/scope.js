ccb05c320d1882a3e60e88b96d0843cc40069663
sharafhamza <kingsharaf14@gmail.com>
2025-03-21 23:40:52 +0600

    new file added

---
+++ Control Flow & Functions/scope/scope.js
@@ -0,0 +1,15 @@
+// function func() {
+//   console.log(x);
+// }
+
+// console.log(x);
+
+// let x = 10;
+// func();
+{
+  for (let i = 2; i <= 5; i++) {
+    console.log(i);
+  }
+}
+
+console.log((i = "sharaf"));
---
+++ Control Flow & Functions/scope/var.js
@@ -0,0 +1,5 @@
+if (true) {
+  var x = 10;
+}
+
+console.log(x);
