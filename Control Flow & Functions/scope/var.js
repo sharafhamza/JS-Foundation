53af887c7a39ac1dda2d058a594221b72767275e
sharafhamza <kingsharaf14@gmail.com>
2025-03-22 23:40:16 +0600

    changes

--- Control Flow & Functions/scope/var.js
+++ Control Flow & Functions/scope/var.js
@@ -1,5 +1,12 @@
 if (true) {
-  var x = 10;
+  // var x = 10;
 }
-
-console.log(x);
+function access() {
+  // access is local to function
+  var x = 10;
+  function test() {
+    console.log(x);
+  }
+  test();
+}
+access();
