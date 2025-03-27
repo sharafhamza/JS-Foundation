069141537f89e6ab32e5b144f9175db72135989c
sharafhamza <kingsharaf14@gmail.com>
2025-03-27 11:00:55 +0600

    problem 3 solved

--- test.js
+++ test.js
@@ -6,14 +6,24 @@
 //   console.log("Number is odd");
 // }
 
-const x = 10;
-const y = 40;
-const z = 30;
+// const x = 10;
+// const y = 40;
+// const z = 30;
 
-if (x < y && x < z) {
-  console.log("x is the smallest number");
-} else if (z < x && z < y) {
-  console.log("z is the smallest number");
-} else if (y < z && y < x) {
-  console.log("y is the smallest number");
-}
+// if (x < y && x < z) {
+//   console.log("x is the smallest number");
+// } else if (z < x && z < y) {
+//   console.log("z is the smallest number");
+// } else if (y < z && y < x) {
+//   console.log("y is the smallest number");
+// }
+
+// const a = 10;
+// const b = 7;
+// const c = 6;
+
+// if (a + b > c && a + c > b && b + c > a) {
+//   console.log("yes you can form a triangle");
+// } else {
+//   console.log("no you cannot");
+// }
