2ac8bcada46406ef5f809edfb22ba2a942a8328d
sharafhamza <kingsharaf14@gmail.com>
2025-03-27 10:49:01 +0600

    problem 2 solved

--- test.js
+++ test.js
@@ -5,3 +5,15 @@
 // } else {
 //   console.log("Number is odd");
 // }
+
+const x = 10;
+const y = 40;
+const z = 30;
+
+if (x < y && x < z) {
+  console.log("x is the smallest number");
+} else if (z < x && z < y) {
+  console.log("z is the smallest number");
+} else if (y < z && y < x) {
+  console.log("y is the smallest number");
+}
