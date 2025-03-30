f4d076db8848cc6e998478d54026d493a75f4670
sharafhamza <kingsharaf14@gmail.com>
2025-03-30 13:34:29 +0600

    changes

--- prototype.js
+++ prototype.js
@@ -1,19 +1,28 @@
-function Car(model, color, year, owner) {
-  this.model = model;
-  this.color = color;
-  this.year = year;
-  this.owner = owner;
-  this.wheel = () => {
-    return 4;
-  };
-}
+// function Car(model, color, year, owner) {
+//   this.model = model;
+//   this.color = color;
+//   this.year = year;
+//   this.owner = owner;
+//   this.wheel = () => {
+//     return 4;
+//   };
+// }
 
-function CarOwner(name, age) {
-  this.name = name;
-  this.age = age;
-}
+// function CarOwner(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
 
-let carOwner = new CarOwner("sharaf", 18);
+// let carOwner = new CarOwner("sharaf", 18);
 
-const Bmw = new Car("BMW", "Black", "2025", carOwner);
-console.log(Bmw.prototype());
+// const Bmw = new Car("BMW", "Black", "2025", carOwner);
+// console.log(Bmw.prototype());
+
+let tesla = {
+  driver: "Ai",
+};
+
+let generaticCar = {
+  tyres: 4,
+  __proto__: tesla,
+};
