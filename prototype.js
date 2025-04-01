c79aabb643a27b1aa5db367646d313bc40fd7df7
sharafhamza <kingsharaf14@gmail.com>
2025-04-01 23:31:18 +0600

    extends and super

--- prototype.js
+++ prototype.js
@@ -14,3 +14,17 @@ class Person {
 
 let person = new Person("Sharaf", "17");
 console.log(person.greet());
+
+class Employee extends Person {
+  constructor(name, age, job) {
+    super(name, age);
+    this.job = job;
+  }
+  work() {
+    console.log(`${this.name} is working as a ${this.job}.`);
+  }
+}
+
+let jane = new Employee("sharaf", 21, "Developer");
+jane.greet();
+jane.work();
