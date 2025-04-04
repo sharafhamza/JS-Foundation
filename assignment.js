cda8b3f572459674853084d0681a13e91865638f
sharafhamza <kingsharaf14@gmail.com>
2025-04-05 05:58:09 +0600

    problem 2 solved

--- assignment.js
+++ assignment.js
@@ -1,8 +1,22 @@
-function simulateAsyncTask() {
-  console.log("Task Started");
-  setTimeout(() => {
-    console.log("Task finished");
-  }, 2000);
+// Problem 1
+// function simulateAsyncTask() {
+//   console.log("Task Started");
+//   setTimeout(() => {
+//     console.log("Task finished");
+//   }, 2000);
+// }
+
+// simulateAsyncTask();
+
+const delays = [1000, 2000, 3000];
+function simulateMultipleTasks() {
+  for (let i = 0; i < delays.length; i++) {
+    let taskNumber = 0 + 1;
+
+    setTimeout(() => {
+      console.log(`Task ${taskNumber} finished`);
+    }, delays[i]);
+  }
 }
 
-simulateAsyncTask();
+simulateMultipleTasks();
