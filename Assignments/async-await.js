34089e67f9fcd4ae0a0ef3f7c02ed94faa14b68f
sharafhamza <kingsharaf14@gmail.com>
2025-04-05 07:38:22 +0600

    solved

--- Assignments/async-await.js
+++ Assignments/async-await.js
@@ -1,10 +1,41 @@
-function fetchUser() {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve("User data"), 1000);
-  }).then((message) => {
-    console.log(message);
-  });
-}
-console.log(fetchUser());
+// Problem 1
+// function fetchUser() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve("User data"), 1000);
+//   });
+// }
 
-function fetchPosts() {}
+// function fetchPosts() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve("User data 2"), 2000);
+//   });
+// }
+// async function fetchAllData() {
+//   const [user, posts] = await Promise.all([fetchUser(), fetchPosts()]);
+//   console.log(user);
+//   console.log(posts);
+// }
+
+// fetchAllData();
+
+// Problem 2 solved
+// function fetchSuccess() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("resolved successfully the data");
+//     }, 1000);
+//   }).then((data) => {
+//     console.log(data);
+//   });
+// }
+
+// fetchSuccess();
+// function fetchFailure() {
+//   return new Promise((_, reject) =>
+//     setTimeout(() => reject("Error occurred"), 1000)
+//   ).catch((error) => {
+//     console.log("One or more promises failed:", error);
+//   });
+// }
+
+// fetchFailure();
