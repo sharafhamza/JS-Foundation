b43dd78bd998aa82564489160073a7935a61b5a7
sharafhamza <kingsharaf14@gmail.com>
2025-04-05 23:56:58 +0600

    building main todo

--- Todo/app.js
+++ Todo/app.js
@@ -1,40 +1,14 @@
-const button = document.querySelector("button");
-let content = document.querySelector(".content");
-const input = document.querySelector("input");
+const button = document.querySelector(".add");
+const input = document.querySelector(".add-input");
+const parentEl = document.querySelector(".main");
+
 const arr = [];
+button.addEventListener("click", () => {
+  const task = input.value;
+  arr.push(task);
+  const li = document.createElement("li");
 
-function removeTodo(item) {
-  const index = arr.indexOf(item);
+  li.textContent = task;
 
-  if (index !== -1) {
-    arr.splice(index, 1);
-  }
-
-  renderList();
-}
-function editTodo(item) {
-  const index = arr.indexOf(item);
-  const existItem = arr[index];
-  input.value = existItem;
-  renderList();
-}
-function renderList() {
-  content.innerHTML = "";
-
-  arr.forEach((item) => {
-    const li = document.createElement("li");
-    li.innerHTML = `${item} <button onClick="editTodo('${item}')">Edit</button> <button onClick="removeTodo('${item}')">Delete</button>`;
-    content.appendChild(li);
-  });
-}
-button.addEventListener("click", function () {
-  if (!input.value) {
-    content.innerHTML = "Please enter a value";
-    return;
-  }
-  arr.push(input.value);
-
-  input.value = "";
-
-  renderList();
+  parentEl.appendChild(li);
 });
