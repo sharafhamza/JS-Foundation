13e865606ea2e909fecce61e77c7d781afc967a1
sharafhamza <kingsharaf14@gmail.com>
2025-04-05 06:36:34 +0600

    inheritance

---
+++ Assignments/Inheritance.js
@@ -0,0 +1,15 @@
+// Problem 1
+// class Animal {
+//   makeSound() {
+//     return "animal make sound";
+//   }
+// }
+
+// class Dog extends Animal {
+//   bark() {
+//     return "Woof!";
+//   }
+// }
+
+// const newAnimal = new Dog();
+// console.log(newAnimal.bark());
--- Assignments/closure.js
+++ Assignments/closure.js
@@ -1,11 +1,16 @@
-function createCounter() {
-  let increment = 0;
-  return function () {
-    return console.log(increment++);
-  };
-}
-let counter = createCounter();
+// Problem 1
+// function createCounter() {
+//   let increment = 0;
+//   return function () {
+//     return console.log(increment++);
+//   };
+// }
+// let counter = createCounter();
 
-counter();
-counter();
-counter();
+// counter();
+// counter();
+// counter();
+
+// Problem 2
+
+function rateLimiter(fn, limit) {}
