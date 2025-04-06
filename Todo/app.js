094a66bb1fa0605067b978f9eecb34df014c2334
sharafhamza <kingsharaf14@gmail.com>
2025-04-06 11:18:51 +0600

    added tasks

--- Todo/app.js
+++ Todo/app.js
@@ -1,14 +1,33 @@
-const button = document.querySelector(".add");
-const input = document.querySelector(".add-input");
-const parentEl = document.querySelector(".main");
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector(".add");
+  const input = document.querySelector(".add-input");
+  const parentEl = document.querySelector(".main");
 
-const arr = [];
-button.addEventListener("click", () => {
-  const task = input.value;
-  arr.push(task);
-  const li = document.createElement("li");
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  li.textContent = task;
+  tasks.forEach((task) => {
+    renderList(task);
+  });
+  button.addEventListener("click", () => {
+    const task = input.value;
+    if (!task) return;
 
-  parentEl.appendChild(li);
+    const newTask = {
+      id: Date.now(),
+      text: task,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTask();
+    input.value = "";
+    console.log(tasks);
+  });
+
+  function renderList(task) {
+    console.log(task);
+  }
+
+  function saveTask() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 });
