aa42a096c92c9c04e50f06e7dc0167dcdae8cdd1
sharafhamza <kingsharaf14@gmail.com>
2025-04-12 12:21:58 +0600

    working on weather app

---
+++ Get Weather App/index.js
@@ -0,0 +1,6 @@
+const cityInput = document.getElementById("cityInput");
+const getWeather = document.getElementById("getWeather");
+
+getWeather.addEventListener("click", () => {
+  console.log("hello");
+});
